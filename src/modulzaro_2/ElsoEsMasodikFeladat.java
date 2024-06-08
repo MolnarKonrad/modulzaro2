09b9173a604aff4e3f1a08d55e2dcf92987945af
Konrad <molnar.konrad@gmail.com>
2024-06-08 09:05:50 +0200

    Metódusok listázása

--- src/modulzaro_2/ElsoEsMasodikFeladat.java
+++ src/modulzaro_2/ElsoEsMasodikFeladat.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package modulzaro_2;
 
-/**
- *
- * @author User
- */
 public class ElsoEsMasodikFeladat {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ProgramozoiTetelek();
+        Adatszerkezet();
+    }
+
+    private static void ProgramozoiTetelek() {
+        velSzam(int also, int felso);
+        feltolt(int db);
+        kiir(int oszlop);
+        progTetelek();
+        progTetelekBemutatasa();
     }
     
 }
